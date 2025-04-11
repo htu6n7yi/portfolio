78063f44a13131d47f62a059efb419e0d221ed93
htu6n7yi <jcavalcanti008@gmail.com>
2025-04-11 19:42:04 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ApresentacaoComponent } from './components/apresentacao/apresentacao.component';
+import { Apresentacao2Component } from './components/apresentacao2/apresentacao2.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, ApresentacaoComponent, Apresentacao2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/apresentacao2/apresentacao2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apresentacao2',
+  imports: [],
+  templateUrl: './apresentacao2.component.html',
+  styleUrl: './apresentacao2.component.css'
+})
+export class Apresentacao2Component {
+
+}
