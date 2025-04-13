41a8ad6c9ef593aab496bf9be1b14c17d79e237a
José Carlos <jcavalcanti008@gmail.com>
2025-04-13 15:05:31 +0000

    build reload

--- docs/server/assets-chunks/index_html.mjs
+++ docs/server/assets-chunks/index_html.mjs
@@ -1,7 +1,7 @@
 export default `<!DOCTYPE html><html lang="pt" data-beasties-container><head>
   <meta charset="utf-8">
   <title>Portfolio</title>
-  <base href="/portfolio/">
+  <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
   <link rel="preconnect" href="https://fonts.googleapis.com">
