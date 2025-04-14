fae5907e92678482f9c34609d1e1e8742d74b950
htu6n7yi <jcavalcanti008@gmail.com>
2025-04-14 17:28:14 -0300

    alinhando responsividade com o menu para telaas menores.

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 1412, hash: 'a35ddcd0f618f7705ae329d8991b6ddd54af9170af452766f24cd7daeb89a16e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1925, hash: '5534fb66589257fbe52c093dab2825f07da563541da9b4d55f904ce0053fa8ff', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 14611, hash: 'cccaba02e9c813f677e54a0da85899eff7b38df6e22ec8c7dcea7701f9f31cb7', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 1412, hash: '442abaf45c4df114680e26aa16770b695d7d0a6e8f5235418941ae91a1cf5e02', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1925, hash: '32f7da7a4c019e99fd48ad92531893e987558479e7f9bd53d36d7936b58e9347', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 14525, hash: '15f407c50126392a68d308f070f6f7d4c6311e8855b1d32fd16ebcc2a9fd9715', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
