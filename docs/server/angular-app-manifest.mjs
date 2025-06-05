4c5f5429b035fbb67a2584b82902d8498b6934f9
htu6n7yi <jcavalcanti008@gmail.com>
2025-06-05 17:33:49 -0300

    atualização dos ícones e mais responsividade

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 1412, hash: '143205d0847b78f7bdeda8414ed1f20d118417eaa3791d7844f1457f319f328b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1925, hash: '6df170b376aaffd8fd518d16ccc91f1f041eaadbedf3454b7094a950572b2818', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 14813, hash: 'b16506da3313d69669c11e91ee13d1d01e2ccbfe7fde13994f6202b03c25f09f', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 1412, hash: '141c030c9bca820f91d26f5edee251852f9b1cbe8305e1e9968e60f9df0b305f', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1925, hash: 'fb6982d54e8aea6a807dc06bf65968274920ebb157d718d79d32933e156043a4', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 17070, hash: '9109fa90957ddc14adf10d0865b40d6e0a965cab0fa14705f9ab946420f63dd9', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
